94546336ba57462e9631b4195898717c66bd4aaa
Jesse Nickles <jesse@littlebizzy.com>
2023-05-30 02:39:16 +0700

    Update options-page-html.php

--- src/partials/options-page-html.php
+++ src/partials/options-page-html.php
@@ -8,11 +8,11 @@ defined( 'ABSPATH' ) || exit;
 	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 	<form action="<?= esc_url( admin_url( 'options.php' ) ) ?>" method="post">
 		<?php
-		settings_fields( 'disable-attachment-pages' );
+		settings_fields( 'redirect-attachment-pages' );
 
-		do_settings_sections( 'disable-attachment-pages' );
+		do_settings_sections( 'redirect-attachment-pages' );
 
-		submit_button( __( 'Save Settings', 'disable-attachment-pages' ) );
+		submit_button( __( 'Save Settings', 'redirect-attachment-pages' ) );
 		?>
 	</form>
 </div>
